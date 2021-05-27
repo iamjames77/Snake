650070162d0b5778fd933fefb4e59fa80f308cbe
iamjames77 <iamjames77@daum.net>
2021-05-28 01:40:50 +0900

    Snake 2단계

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #include <vector>
 #include <algorithm>
 #include <locale.h>
+#include <unistd.h>
 #include "Game.h"
 using namespace std;
+
+bool kbhit();
 
 int main()
 {
@@ -12,7 +15,54 @@ int main()
   setlocale(LC_ALL, "");
   initscr();
   keypad(stdscr, TRUE);
-  int key;
+  int key = KEY_LEFT;
+  int a;
+  int i = 21;
   g1.printmap();
+  while(1)
+  {
+    if(kbhit())
+    {
+      if((a=getch())!=key){key =a;}
+      else
+      {
+        continue;
+      }
+    }
+    switch (key){
+      case KEY_LEFT:
+          g1.SnakeLeft();
+          break;
+      case KEY_RIGHT:
+          g1.SnakeRight();
+          break;
+      case KEY_UP:
+          g1.SnakeUp();
+          break;
+      case KEY_DOWN:
+          g1.SnakeDown();
+          break;
+      }
+    usleep(500000);
+  }
   endwin();
+}
+
+bool kbhit(){
+  int ch;
+  bool ret;
+
+  nodelay(stdscr, TRUE);
+
+  ch = getch();
+  if(ch==ERR){
+    ret = false;
+  }
+  else{
+    ret = true;
+    ungetch(ch);
+  }
+
+  nodelay(stdscr,FALSE);
+  return ret;
 }
