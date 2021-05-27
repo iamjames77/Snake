a7e058ada9f8b6976cd4ad35d26be111e56ed275
iamjames77 <iamjames77@daum.net>
2021-05-28 01:59:30 +0900

    Snake 2단계

--- Game.h
+++ Game.h
@@ -8,11 +8,11 @@ using namespace std;
 class Game{
   int snakesize;
   int **map;
-  int row;
-  int col;
   vector<pair<int, int>> v;
   vector<pair<int, int>>::iterator it;
 public:
+  int row;
+  int col;
   Game(int r = 21, int c =21);
   void MakeMap();
   void printmap();
