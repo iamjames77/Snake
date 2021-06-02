4be9e6fc2405af912ebf86be43dc3ca245a986de
iamjames77 <66625994+iamjames77@users.noreply.github.com>
2021-06-03 02:27:13 +0900

    Update Game.h

--- Game.h
+++ Game.h
@@ -27,18 +27,18 @@ class Game{
   vector<pair<int, int>>::iterator it;
 public:
   Game(int r = 21, int c =21, int level = 1);
-  void Play();
-  bool kbhit();
+  void Play(); // Game 실행 함수
+  bool kbhit(); // 키보드 입력확인 함수
   void MakeMap(); //Map 생성
-  void GameSuccess();
-  void GameFail();
-  void LevelUp(int lv);
+  void GameSuccess();//게임 성공시 윈도우 출력
+  void GameFail();//게임 실패시 윈도우 출력
+  void LevelUp(int lv);//map level에 따라 map 수정
   void printmap(); //Map 출력
   void SnakeLoc(); //Snake의 위치 정보 업데이트
-  void SnakeMove(int* key);
-  void Gate();
-  void GateNotOnTheWall(int* x, int *y,int **key,int data);
-  void Growth();
-  void Poison();
-  void scoreboard();
+  void SnakeMove(int* key);//Snake 이동함수
+  void Gate();//Gate 생성 함수
+  void GateNotOnTheWall(int* x, int *y,int **key,int data); //Gate 외벽이 아닌 내벽에 있을 시 실행되는 함수
+  void Growth();//Snake 길이 증가
+  void Poison();//Snake 길이 감소
+  void scoreboard();//점수판 
 };
